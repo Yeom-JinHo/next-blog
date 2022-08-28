c4fac08754a5d1f0504f548d555a7de2e694fc45
Yeom-JinHo <pwlsghq@naver.com>
2022-08-28 21:14:59 +0900

    fix : header li tag

--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -14,6 +14,8 @@ const Navigation = () => {
       <ul>
         <li>
           <Link href="/posts">Posts</Link>
+        </li>
+        <li>
           <Link href="/contact">Contact</Link>
         </li>
       </ul>
