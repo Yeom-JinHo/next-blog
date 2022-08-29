0a8348ac8e09f73d6530b5fb0a07158aa93c395f
Yeom-JinHo <pwlsghq@naver.com>
2022-08-29 11:02:15 +0900

    feat : contatc notification

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,21 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Notification from "../ui/notification";
 import classes from "./contact-form.module.css";
 
 const ContactForm = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [msg, setMsg] = useState("");
+  const [reqStatus, setReqStatus] = useState();
 
-  function sendMsgHanlder(e) {
+  useEffect(() => {
+    if (reqStatus === "success" || reqStatus === "error") {
+      const timer = setTimeout(() => {
+        setReqStatus(null);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [reqStatus]);
+  async function sendMsgHanlder(e) {
     e.preventDefault();
 
-    fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify({ email, name, msg }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    setReqStatus("pending");
+    try {
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify({ email, name, msg }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+      setReqStatus("success");
+      setEmail("");
+      setName("");
+      setMsg("");
+    } catch (e) {
+      setReqStatus("error");
+      throw new Error(e.message || "뭔가 잘못되었어요!");
+    }
+  }
+  let notification;
+  if (reqStatus === "pending") {
+    notification = {
+      status: "pending",
+      title: "메세지가 전송중",
+      message: "잠시만 가디려주세요.",
+    };
+  }
+  if (reqStatus === "success") {
+    notification = {
+      status: "success",
+      title: "메세지 전송성공!",
+      message: "메세지 전송에 성공하였습니다.",
+    };
+  }
+  if (reqStatus === "error") {
+    notification = {
+      status: "error",
+      title: "메세지 전송실패!",
+      message: "다시 한번 시도해주세요.",
+    };
   }
   return (
     <section className={classes.contact}>
@@ -57,6 +101,7 @@ const ContactForm = () => {
           <button>메세지 전송</button>
         </div>
       </form>
+      {notification && <Notification noti={notification} />}
     </section>
   );
 };
